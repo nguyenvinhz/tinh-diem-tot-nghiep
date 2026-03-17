747b26f5cecb488971bb9ee4b0116e8a0e0a0de7
nguyenvinhz <nguyenvinh071206@gmail.com>
2026-03-17 23:40:03 +0700

    Create ping.py

---
+++ ping.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=options)
+
+driver.get("https://tinh-diem-tot-nghiep.streamlit.app")
+time.sleep(45)
+driver.quit()
